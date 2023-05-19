02d25c2f9edfc37c7e3b8260dbb84bc291964f7a
Kasmodan1 <Kasmodan1@yahoo.com>
2023-05-18 20:26:29 -0700

    addming more js

--- Lesson5/scripts/bom.js
+++ Lesson5/scripts/bom.js
@@ -5,10 +5,23 @@ const listElement = document.querySelector("ul");
 buttonElement.addEventListener('click', function() {
   const inputValue = inputElement.value.trim();
 
-  if (inputValue == "") {
-    console.log("Imput is blank, go for it!");
+  if (inputValue !== "") {
+    console.log("Imput is not blank");
+
+    const liElement = document.createElement("li");
+    liElement.textContent = inputElement;
   }
   else {
-    console.log("input is not blank");
+    console.log("input is blank");
+    
+
   }
+
+
+  //listElement.appendChild(liElement);
+
+  const deleteButton = document.createElement("button");
+
+
+
 });
