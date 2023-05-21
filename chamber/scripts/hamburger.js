7d2a50551fce044fdf2d063a570ba5f33e0837d2
Kasmodan1 <Kasmodan1@yahoo.com>
2023-05-20 22:30:00 -0700

    finnally fixed side nav overlay!

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,6 +1,34 @@
 const hambutton = document.querySelector(".hamburger");
-hambutton.addEventListener("click", toggleMenu, false);
+const navigation = document.querySelector(".navi");
+const closeMenu = document.querySelector(".closebtn");
 
+//*** Adding event Listeners ***************
+hambutton.addEventListener("click", openNav, false);
+closeMenu.addEventListener("click", closeNav, false);
+
+// ToggleMenu fuction  **********************
 function toggleMenu() {
-  document.querySelector(".navi").classList.toggle("responsive");
+  navigation.classList.toggle("responsive");
 }
+
+//Side Nav overlay fuctions ******************
+function openNav() {
+  document.getElementById("mySidenav").style.width = "220px";
+  document.getElementById("main").classList.add("pushed");
+  document.getElementById("header").classList.add("pushed");
+  document.getElementById("footer").classList.add("pushed");
+}
+  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").classList.remove("pushed");
+  document.getElementById("header").classList.remove("pushed");
+  document.getElementById("footer").classList.remove("pushed");
+}
+
+// This is to get the date displayed ***********
+const datefield = document.querySelector("#datefield");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
+
