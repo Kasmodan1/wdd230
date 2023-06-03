ce25488a718b5bce34cb3310708fb784b01e1804
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-02 21:20:21 -0700

    updated the html and js

--- Lesson7/scripts/lazyload.js
+++ Lesson7/scripts/lazyload.js
@@ -1,7 +1,34 @@
 
-const placeholderImage = document.querySelectorAll('.placeholder-image[data-src]')
+// This is the load the placeholder image first
+const placeholderImages = document.querySelectorAll('.placeholder-image[data-src]');
+const placeObserver = new IntersectionObserver((entries, placeObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const placeholder = entry.target;
+      const img = placeholder;
+      const src = img.getAttribute('data-src');
+      
+      // Set the placeholder image as the source initially
+      placeholder.style.backgroundImage = `url("${src}")`;
 
-const placeObserver = new IntersectionObserver((entries, observer) => {
+      //takes focus away
+      placeObserver.unobserve(placeholder);
+    }
+  });
+
+});
+
+placeholderImages.forEach(placeholder => {
+  placeObserver.observe(placeholder);
+});
+
+
+
+
+// this loads my real image that I wanted to load.
+const lazyImages = document.querySelectorAll('.lazy-image');
+
+const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       const img = entry.target;
@@ -12,54 +39,19 @@ const placeObserver = new IntersectionObserver((entries, observer) => {
         newImage.onload = () => {
           img.setAttribute('src', src);
           img.classList.add('loaded');
+          
           // Hide the placeholder image
-          //const placeholder = img.previousElementSibling;
-          //if (placeholder && placeholder.classList.contains('placeholder-image')) {
-          //  placeholder.style.display = 'none';
-          //
+          const placeholder = img.previousElementSibling;
+          if (placeholder && placeholder.classList.contains('placeholder-image')) {
+            placeholder.style.display = 'none';
+          }
         };
       }
-      placeObserver.unobserve(img);
+      observer.unobserve(img);
     }
   });
 });
 
-placeholderImage.forEach(image => {
-  placeObserver.observe(image);
+lazyImages.forEach(image => {
+  observer.observe(image);
 });
-
-
-
-
-
-
-//const lazyImages = document.querySelectorAll('.lazy-image');
-//
-//const observer = new IntersectionObserver((entries, observer) => {
-//  entries.forEach(entry => {
-//    if (entry.isIntersecting) {
-//      const img = entry.target;
-//      const src = img.getAttribute('data-src');
-//      if (src) {
-//        const newImage = new Image();
-//        newImage.src = src;
-//        newImage.onload = () => {
-//          img.setAttribute('src', src);
-//          img.classList.add('loaded');
-//          // Hide the placeholder image
-//          const placeholder = img.previousElementSibling;
-//          if (placeholder && placeholder.classList.contains('placeholder-image')) {
-//            placeholder.style.display = 'none';
-//          }
-//        };
-//      }
-//      observer.unobserve(img);
-//    }
-//  });
-//});
-//
-//lazyImages.forEach(image => {
-//  observer.observe(image);
-//});
-//
-
