6d2e80e2e30378828eb1b31b0412102074f7c4cf
Kasmodan1 <Kasmodan1@yahoo.com>
2023-07-05 19:15:15 -0700

    fixed menu js error that was outputting to term

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,44 +1,50 @@
 const hambutton = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navi");
 const closeMenu = document.querySelector(".closebtn");
-
 //*** Adding event Listeners ***************
 hambutton.addEventListener("click", openNav, false);
 closeMenu.addEventListener("click", closeNav, false);
-
 // ToggleMenu function  **********************
 function toggleMenu() {
   navigation.classList.toggle("responsive");
-
 }
-
 // Join Chambers button function
 function buttonClick() {
   // point it to the join.html page
   window.location.href = "join.html";
-
 }
-
 //Side Nav overlay functions ******************
 function openNav() {
-  document.getElementById("mySidenav").style.width = "240px";
-  document.getElementById("main").classList.add("pushed");
-  document.getElementById("header").classList.add("pushed");
-  document.getElementById("footer").classList.add("pushed");
-
+  const sideNav = document.getElementById("mySidenav");
+  const mainContent = document.getElementById("main");
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+  sideNav.style.width = "240px";
+  if (mainContent) {
+    mainContent.classList.add("pushed");
+  }
+  if (header) {
+    header.classList.add("pushed");
+  }
+  if (footer) {
+    footer.classList.add("pushed");
+  }
 }
-  
+// Side Nav overlay functions
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").classList.remove("pushed");
-  document.getElementById("header").classList.remove("pushed");
-  document.getElementById("footer").classList.remove("pushed");
-
+  const sideNav = document.getElementById("mySidenav");
+  const mainContent = document.getElementById("main");
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+  sideNav.style.width = "0";
+  if (mainContent) {
+    mainContent.classList.remove("pushed");
+  }
+  if (header) {
+    header.classList.remove("pushed");
+  }
+  if (footer) {
+    footer.classList.remove("pushed");
+  }
 }
-
-// This is to get the date displayed ***********
-//const datefield = document.querySelector("#datefield");
-//const now = new Date();
-//const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-//datefield.textContent = fulldate;
 
