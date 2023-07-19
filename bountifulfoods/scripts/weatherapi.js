41b0e3ce9890fadcd1ee0797ea7b35e4ca3d0cbf
Kasmodan1 <Kasmodan1@yahoo.com>
2023-07-19 02:20:55 -0700

    had to create js to get 3 days from bad api

--- bountifulfoods/scripts/weatherapi.js
+++ bountifulfoods/scripts/weatherapi.js
@@ -37,18 +37,26 @@ document.addEventListener('DOMContentLoaded', function () {
   function displayForecast(forecastData) {
     const forecastElement = document.getElementById('forecast');
     forecastElement.innerHTML = ''; // Clear previous forecast data
-
+  
     const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-    // Process forecast data to get daily min and max temperatures
-    const dailyForecast = processForecastData(forecastData);
-
+  
+    // Filter out forecast data for the next three days
+    const now = new Date();
+    const nextThreeDays = [now, new Date(now.getTime() + 24 * 60 * 60 * 1000), new Date(now.getTime() + 2 * 24 * 60 * 60 * 1000)];
+    const filteredForecastData = forecastData.list.filter((forecast) => {
+      const forecastDate = new Date(forecast.dt * 1000);
+      return nextThreeDays.some((day) => day.toDateString() === forecastDate.toDateString());
+    });
+  
+    // Process filtered forecast data to get daily min and max temperatures
+    const dailyForecast = processForecastData(filteredForecastData);
+  
     for (const day in dailyForecast) {
       const { minTemp, maxTemp } = dailyForecast[day];
       const forecastDate = new Date(day);
       const dayIndex = forecastDate.getDay(); // Get the day index (0 to 6)
       const dayName = weekdays[dayIndex]; // Get the short weekday name using the index
-
+  
       const forecastItem = document.createElement('div');
       forecastItem.classList.add('forecast-item');
       forecastItem.innerHTML = `
@@ -63,15 +71,19 @@ document.addEventListener('DOMContentLoaded', function () {
   // Function to display current weather
   function displayCurrentWeather(currentWeatherData) {
     const currentWeatherElement = document.getElementById('current-weather');
+    //console.log('Current Weather Data:', currentWeatherData);
+
     const temperature = currentWeatherData.main.temp;
     const condition = currentWeatherData.weather[0].description;
     const humidity = currentWeatherData.main.humidity;
 
-    currentWeatherElement.innerHTML = `
-      <p>Temperature: ${temperature.toFixed(1)}°F</p>
-      <p>Condition: ${condition}</p>
-      <p>Humidity: ${humidity}%</p>
-    `;
+    const temperatureElement = document.getElementById('temperature');
+    const conditionElement = document.getElementById('condition');
+    const humidityElement = document.getElementById('humidity');
+
+    temperatureElement.textContent = `${temperature.toFixed(1)}°F`;
+    conditionElement.textContent = condition;
+    humidityElement.textContent = `${humidity}%`;
   }
 
   // Async function to fetch weather data and display
@@ -81,15 +93,19 @@ document.addEventListener('DOMContentLoaded', function () {
       const currentWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}&units=imperial`);
       const currentWeatherData = await currentWeatherResponse.json();
 
+      console.log('Forecast Data:', currentWeatherData); // Add this line to log
+
       // Fetch 5-day forecast data in imperial units (Fahrenheit)
       const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}&units=imperial`);
       const forecastData = await forecastResponse.json();
+
+      console.log('Forecast Data:', forecastData); // Add this line to log
 
       // Display current weather
       displayCurrentWeather(currentWeatherData);
 
       // Display three-day forecast
-      displayForecast(forecastData.list);
+      displayForecast(forecastData);
     } catch (error) {
       console.error('Error fetching weather data:', error);
     }
